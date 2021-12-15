1a958f3d039630645ba3e581b73df2060f344029
Pranav Talmale <pranavtalmale@gmail.com>
2021-12-15 15:19:01 +0530

    remove prebuilt stuff and switch to ArrowOS kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,5 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/alioth
 TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 
 
